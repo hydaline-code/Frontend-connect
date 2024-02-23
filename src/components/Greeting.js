3bb3bedea956cc538f5086e14e95be9c08286e91
hydaline-code <djougancharlen@gmail.com>
2024-02-23 13:58:42 +0100

    Add routes and component

---
+++ src/components/Greeting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greeting = () => {
+  return (
+    <div className="greeting-container">
+      <h1 className="greeting-title">Welcome to our Hello Rails-React in one app !</h1>
+      <div className="greeting-content" />
+    </div>
+  );
+};
+export default Greeting;
